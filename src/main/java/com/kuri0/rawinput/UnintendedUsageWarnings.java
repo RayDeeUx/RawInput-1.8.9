b453efaaa79b0cb5b0ba1aae6308f88c929e5fad
RayDeeUx <raydeeux@gmail.com>
2022-07-30 13:48:33 -0400

    1.1.3

--- src/main/java/com/kuri0/rawinput/UnintendedUsageWarnings.java
+++ src/main/java/com/kuri0/rawinput/UnintendedUsageWarnings.java
@@ -1,6 +1,7 @@
 package com.kuri0.rawinput;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.Util;
@@ -24,12 +25,15 @@ public class UnintendedUsageWarnings {
     private boolean hasSentWarningForSession = false;
     private String warningMessage = "his mod is only intended for Windows. Please remove this mod from your mods folder and relaunch your game as soon as possible, as this mod is dormant when used outside of Windows.";
     private String warningMessagePrefix = "[RawInput] T";
+    private String os = System.getProperty("os.name");
+    private EntityPlayerSP mcgmtp = Minecraft.getMinecraft().thePlayer;
 
     @SubscribeEvent
     public void sendWarning(TickEvent.ClientTickEvent event){
         if (hasSentWarningForSession || (Util.getOSType() == Util.EnumOS.WINDOWS) || Minecraft.getMinecraft().thePlayer == null) return;
         if ((Loader.isModLoaded("skyclientcosmetics") || Loader.isModLoaded("skyblockclientupdater"))) warningMessagePrefix = "[RawInput] Hi there! This is another reminder that t";
-        Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + warningMessagePrefix + warningMessage));
+        mcgmtp.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + warningMessagePrefix + warningMessage));
+        mcgmtp.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "[RawInput] You are currently on " + os + ". If you feel that this message was a mistake, please ping Erymanthus#5074 in the SkyClient Discord server: https://inv.wtf/skyclient"));
         //prevent sending warning more than once per session
         hasSentWarningForSession = true;
     }
